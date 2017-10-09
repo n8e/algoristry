043154f5bc66e1a7ae03d84e9444dd8007816635
n8e <godmetweenciati@gmail.com>
2017-10-09 14:02:31 +0300

    introducing generators

--- generator.js
+++ generator.js
@@ -1,33 +1,35 @@
-const faker = require('faker');
-const fs = require('fs');
+const faker = require("faker");
+const fs = require("fs");
 
-const createUsers = (num) => {
-    let users = [];
-    for (let i = 0; i < num; i += 1) {
-        users.push({
-            firstName: faker.name.firstName(),
-            lastName: faker.name.lastName(),
-            jobTitle: faker.name.jobTitle(),
-            email: faker.internet.email(),
-            telephone: faker.phone.phoneNumber()
-        });
-    }
-    return JSON.stringify(users);
-}
+const createUsers = num => {
+  let users = [];
+  for (let i = 0; i < num; i += 1) {
+    users.push({
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      jobTitle: faker.name.jobTitle(),
+      email: faker.internet.email(),
+      telephone: faker.phone.phoneNumber()
+    });
+  }
+  return JSON.stringify(users);
+};
 
 const generateUsers = () => {
-    // check if users exists
-    fs.readFile('./json/users.json', 'utf8', (err, data) => {
-        if (err) {
-            // create users
-            fs.writeFile('./json/users.json', createUsers(100), (err, data) => {
-                if (err) return console.log(err);
-                console.log('File created!');
-            });
-            return console.log('Error reading "./json/users.json". New file being created...');
-        }
-        return console.log('File found! Not duplicated.')
-    });
-}
+  // check if users exists
+  fs.readFile("./json/users.json", "utf8", (err, data) => {
+    if (err) {
+      // create users
+      fs.writeFile("./json/users.json", createUsers(100), (err, data) => {
+        if (err) return console.log(err);
+        console.log("File created!");
+      });
+      return console.log(
+        'Error reading "./json/users.json". New file being created...'
+      );
+    }
+    return console.log("File found! Not duplicated.");
+  });
+};
 
 generateUsers();
