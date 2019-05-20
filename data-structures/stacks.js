2fea15ae8223b24720895526c5c107955e7e920d
n8e <godmetweenciati@gmail.com>
2019-05-20 20:55:59 +0300

    Stacks

---
+++ data-structures/stacks.js
@@ -0,0 +1,39 @@
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  size() {
+    return this.data.length;
+  }
+
+  first() {
+    return this.size() ? this.data[0] : 'No elements in Stack';
+  }
+
+  last() {
+    return this.size() ? this.data[this.data.length - 1] : 'No elements in Stack';
+  }
+
+  stack(record) {
+    this.data.unshift(record);
+  }
+
+  unstack() {
+    this.data.pop();
+  }
+}
+
+let myStack = new Stack();
+
+myStack.stack(21);
+myStack.stack(32);
+myStack.stack(43);
+myStack.stack(54);
+myStack.stack(65);
+myStack.stack(76);
+myStack.unstack();
+
+console.log('Size:', myStack.size());
+console.log('First:', myStack.first());
+console.log('Last:', myStack.last());
